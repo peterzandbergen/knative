ca7a82d4dd78153a40883033fceae79a1ebb4cd8
peterzandbergen <peter.zandbergen@myhops.com>
2018-11-07 22:42:55 +0100

    Delete script

--- delete-cluster.sh
+++ delete-cluster.sh
@@ -2,8 +2,21 @@
 
 # Based on https://github.com/knative/docs/blob/master/install/Knative-with-GKE.md
 
-# Create cluster in script
-CLUSTER_NAME=knative
-CLUSTER_ZONE=europe-west4-a
+# Default cluster settings
+DEF_CLUSTER_NAME=knative
+DEF_CLUSTER_ZONE=europe-west4-a
+
+if [ -z ${CLUSTER_NAME} ] ; then
+    CLUSTER_NAME=${DEF_CLUSTER_NAME}
+fi
+
+if [ -z ${CLUSTER_ZONE} ] ; then
+    CLUSTER_ZONE=${DEF_CLUSTER_ZONE}
+fi
+
+echo $CLUSTER_NAME
+echo $CLUSTER_ZONE
+
+sleep 30
 
 gcloud container clusters delete $CLUSTER_NAME --zone $CLUSTER_ZONE
